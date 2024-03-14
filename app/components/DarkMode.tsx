3cebea9d9c6147e3c8c267dbbe416c72aba1af9d
keneogu <kc101com@gmail.com>
2024-03-14 12:32:48 +0100

    only load the mode switch when mounted

--- app/components/DarkMode.tsx
+++ app/components/DarkMode.tsx
@@ -2,14 +2,20 @@
 import { MdLightMode } from "react-icons/md";
 import { BsFillMoonFill } from "react-icons/bs";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function DarkMode() {
   const { systemTheme, theme, setTheme } = useTheme();
-
+  const [mounted, setMounted] = useState(false);
   const currentTheme = theme === "system" ? systemTheme : theme;
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+  
   return (
     <div>
-      {currentTheme === "dark" ? (
+      {mounted && (currentTheme === "dark" ? (
         <MdLightMode
           className="text-xl cursor-pointer hover:text-amber-500"
           onClick={() => setTheme("light")}
@@ -19,7 +25,7 @@ export default function DarkMode() {
           className="text-xl cursor-pointer hover:text-amber-500"
           onClick={() => setTheme("dark")}
         />
-      )}
+      ))}
     </div>
   );
 }
