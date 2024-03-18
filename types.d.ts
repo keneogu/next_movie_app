28dbb4ab7ef435b972df6b3a932b33faf395dd22
keneogu <kc101com@gmail.com>
2024-03-18 22:17:19 +0100

    add MovieItems api types

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,21 @@
 // import { IconType } from "react-icons";
 
 type MenuItem = {
-  title: string,
-  address: string,
+  title: string;
+  address: string;
   Icon?: any;
-}
+};
+
+type MovieItems = {
+  results: {
+    id?: number;
+    title?: string;
+    name?: string;
+    original_name?: string;
+    overview?: string;
+    poster_path?: string;
+    release_date?: string;
+    vote_average?: number;
+    vote_count?: number;
+  }[];
+};
