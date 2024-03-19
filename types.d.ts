940f177eeda6e3457b21570ef00faa61c307f4a1
keneogu <kc101com@gmail.com>
2024-03-19 23:00:20 +0100

    add the movie dynamic route id

--- types.d.ts
+++ types.d.ts
@@ -14,6 +14,7 @@ type MovieItems = {
     original_name?: string;
     overview?: string;
     poster_path?: string;
+    backdrop_path?: string,
     release_date?: string;
     vote_average?: number;
     vote_count?: number;
